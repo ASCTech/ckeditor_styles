2739104384bd45c3443f054073f5d0930df7f5e3
Mike Butsko <butsko.7@osu.edu>
2012-02-08 14:07:34 -0500

    Initial commit.

---
+++ ckeditor_styles.js
@@ -0,0 +1,8 @@
+CKEDITOR.addStylesSet('my_styles',
+    [
+    // Inline Styles
+    { name : 'Normal' , element : 'p' },
+    { name : 'Heading 2' , element : 'h2' },
+    { name : 'Heading 3' , element : 'h3' }
+    ]
+    );
